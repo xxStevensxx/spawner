325e274f13f330982f3f6a87f66acf2c81d7e374
Stevens <29457062+xxStevensxx@users.noreply.github.com>
2025-02-15 03:30:29 +0100

    optimisation du code et suppression des tanks quand ils sortent de l'ecran

--- main.lua
+++ main.lua
@@ -9,13 +9,16 @@ io.stdout:setvbuf("no")
 local time = 0
 local sable = 1
 local sablier = sable
+local angle = 0
+local imgEnnemi = love.graphics.newImage("/assets/tank.png")
+local lstZoneSpawn = {}
+local ennemiWidth = imgEnnemi:getWidth()
+local ennemiHeight = imgEnnemi:getHeight()
 
 local lstEnnemi = {
     angle = 0
 }
-local imgEnnemi = love.graphics.newImage("/assets/tank.png")
 
-local lstZoneSpawn = {}
 
 lstZoneSpawn[1] = {
     x = love.graphics.getWidth() / 2,
@@ -63,23 +66,36 @@ function love.update(dt)
         sablier = sable
     end
 
-    for nb = 1, #lstEnnemi do 
-        if lstEnnemi[nb].angle == math.pi / 2 then
-                lstEnnemi[nb].y = lstEnnemi[nb].y + 120 * dt
+    --On parcours a l'envers car on à de la suppression de tanks
+    for nb = 1, #lstEnnemi -1 do 
 
-        elseif lstEnnemi[nb].angle == 0 then
-            lstEnnemi[nb].x = lstEnnemi[nb].x + 120 * dt
-        elseif lstEnnemi[nb].angle == math.pi * 1.5 then
-            lstEnnemi[nb].y = lstEnnemi[nb].y - 120 * dt
+        local vx = 120 * math.cos(lstEnnemi[nb].angle) * dt
+        local vy = 120 * math.sin(lstEnnemi[nb].angle) * dt
+        local ennemi = lstEnnemi[nb]
+
+        ennemi.x = ennemi.x + vx
+        ennemi.y = ennemi.y + vy
+
+        -- effet tank ivre 😁
+        ennemi.angle = ennemi.angle + math.random(-20, 20) / 100
+
+
+        --suppresion tank qui sort de l'ecran 😥
+        if ennemi.x + ennemiWidth / 2 < 0 or
+         ennemi.x + ennemiWidth / 2 > love.graphics.getWidth() or
+          ennemi.y + ennemiHeight / 2 < 0 or
+           ennemi.y + ennemiHeight / 2 > love.graphics.getHeight() then
+
+            table.remove(lstEnnemi, nb)
         end
+
+        
     end
 
 
 end
 
 function love.draw()
-    -- love.graphics.print(time)
-    -- love.graphics.print(sablier)
 
     for zone = 1, #lstZoneSpawn do 
         love.graphics.rectangle("fill", lstZoneSpawn[zone].x, lstZoneSpawn[zone].y, 32, 32)
@@ -87,6 +103,7 @@ function love.draw()
 
     for nb = 1, #lstEnnemi do 
         love.graphics.draw(imgEnnemi, lstEnnemi[nb].x, lstEnnemi[nb].y, lstEnnemi[nb].angle, 1, 1, imgEnnemi:getHeight() / 2, imgEnnemi:getWidth() / 2)
+        love.graphics.print("nb of tank(s): "..tostring(#lstEnnemi))
     end
 end
 
